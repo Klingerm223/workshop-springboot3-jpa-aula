0224aec9f00a1f5751fe954b8e7dd9548fedac56
Klinger Marinho <klingercm@hotmail.com>
2023-09-02 19:09:45 -0400

    Category

---
+++ src/main/java/com/educandoweb/cursospring/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.cursospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.cursospring.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+	
+	
+}
---
+++ src/main/java/com/educandoweb/cursospring/services/CategoryService.java
@@ -0,0 +1,30 @@
+package com.educandoweb.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.cursospring.entities.Category;
+import com.educandoweb.cursospring.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository repository;
+	
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById (Long Id) {
+		
+		Optional <Category> obj = repository.findById(Id);
+		return obj.get();
+		
+	}
+	
+	
+}
